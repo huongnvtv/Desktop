d98987457ae5293dd07079eba79cc8dc4b0fcc4b
huongnvtv <huongitnv@gmail.com>
2021-07-01 20:42:09 +0700

    commit all

--- DevGit/HTML-DOM/ontap/main1.js
+++ DevGit/HTML-DOM/ontap/main1.js
@@ -1,3 +1,10 @@
+// 1.Get list elements li là con trực tiếp của ul
+// thứ nhất và lưu vào biến listItems1
+// 2.Get list elements li là con trực tiếp của ul 
+// thứ hai và lưu vào biến listItems2
+// 3.Get list elements li là con trực tiếp của ul 
+// thứ nhất và ul thứ hai sau đó lưu vào biến listItems3
+
 var listItems1 =document.querySelectorAll('.parent > li');
 console.log(listItems1);
 var listItems2 =document.querySelectorAll(".parent > ul >li")
