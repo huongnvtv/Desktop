0cf86a1b7e49a4caa49bb81e3807eba51eca26a5
huongnvtv <huongitnv@gmail.com>
2021-07-02 20:39:30 +0700

    commit all

--- DevGit/HTML-DOM/ontap/main2.js
+++ DevGit/HTML-DOM/ontap/main2.js
@@ -1,3 +1,6 @@
+// 1. Get element có class box và lưu vào biến boxElement
+// 2. Từ biến boxElement get elements có class children 
+// lưu vào biến childrenElements
 var boxElement =document.querySelector('.box')
 console.log(boxElement)
 var childrenElements =boxElement.getElementsByClassName('children')
--- DevGit/HTML-DOM/ontap/main3.js
+++ DevGit/HTML-DOM/ontap/main3.js
@@ -3,5 +3,10 @@
 // var getFirstElement =getElement.getElementsByTagName('td');
 // console.log(getFirstElement[0])
 // cách 1 :lấy ra element 
-var getElement =document.getElementsByTagName("tr")[1];
-console.log(getElement)
+var trElement =document.getElementsByTagName("tr");
+console.log(trElement)
+var firstTrElement = trElement[0];
+console.log([firstTrElement])
+console.log(firstTrElement.cells)
+console.log(firstTrElement.localName)
+console.log(firstTrElement.children)
