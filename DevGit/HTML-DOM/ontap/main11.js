57256c480e1bca6c563b18724fd352a536d9b532
huongnvtv <huongitnv@gmail.com>
2021-07-05 15:23:50 +0700

    commit all

---
+++ DevGit/HTML-DOM/ontap/main11.js
@@ -0,0 +1,16 @@
+// xóa element
+var inputElements = document.querySelectorAll('input[type = "button"]');
+console.log(inputElements);
+var n = inputElements.length;
+for(var i = 0; i < inputElements.length; i++){
+   inputElements[i].addEventListener('click',function(){
+       var grandParentElement = this.parentElement.parentElement;
+       grandParentElement.remove();// dùng để xóa 1 element
+       // this trỏ đến đối tượng đang xét == e.target
+       n--;
+       if(n == 0){
+           
+       }
+   })
+}
+
