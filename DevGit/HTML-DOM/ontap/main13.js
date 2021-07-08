c17b4a2a7e04610daf5536c239e70438f6a3acec
huongnvtv <huongitnv@gmail.com>
2021-07-08 07:47:13 +0700

    commit all

--- DevGit/HTML-DOM/ontap/main13.js
+++ DevGit/HTML-DOM/ontap/main13.js
@@ -1,9 +1,14 @@
 // viết chương trình javaScript thêm hàng vào bảng 
 var tableElement = document.getElementById('sampleTable');
 console.log([tableElement])
+var k = tableElement.children.length;
 function insert_Row(){
-    var trElementEnd = tableElement.children[0].lastElementChild;
     var i = tableElement.children.length +1;
     var html = "<tr><td>Hàng " + (i+1) +" ô1</td><td> Hàng " +(i+1)+" ô2</td></tr>"
     tableElement.insertAdjacentHTML('beforeend',html)
+    k = i
+}
+function delete_Row(){
+    var trElement = tableElement.lastElementChild;
+    trElement.remove();
 }
