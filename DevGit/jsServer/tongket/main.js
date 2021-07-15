eb74b02da1b030213dfe030061b48564e920a6ab
huongnvtv <huongitnv@gmail.com>
2021-07-15 08:57:04 +0700

    tongket part 1

--- DevGit/jsServer/tongket/main.js
+++ DevGit/jsServer/tongket/main.js
@@ -1,1 +1,15 @@
 // xây dựng một ứng dụng quản lí sinh viên qua từng lớp
+var classAPI = "http://localhost:3000/class";
+function start(){
+    getListClass();
+}
+function getListClass(){
+    fetch(classAPI)
+       .then(function(respone){
+            return respone.json();
+       })
+       .then(function(data){
+           console.log(data)
+       })
+}
+start();
