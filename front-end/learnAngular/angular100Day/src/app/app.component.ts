4f1f00acb1daaf166189f28e25fa562868735122
huongnvtv <huongitnv@gmail.com>
2021-09-26 10:52:18 +0700

    commit code zing

--- front-end/learnAngular/angular100Day/src/app/app.component.ts
+++ front-end/learnAngular/angular100Day/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular100Day';
+  // các cách đẩy dữ liệu ra file app.component.html
+  // cách 1 dùng {{}} (property biding)
+  // cách 2 sử dụng data binding (property biding)
+  inputType = 'text';
+  name = 'huong';
+  // cách 3 sử dụng event binding
+  demo() {
+    console.log('event', event);
+  }
 }
