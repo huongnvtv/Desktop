4026bca4c7355ae2a71bfb9444ee8d511aeef6f3
huongnvtv <huongitnv@gmail.com>
2021-10-02 16:32:23 +0700

    ng conent in angular

--- front-end/learnAngular/angular100Day/src/app/app.component.ts
+++ front-end/learnAngular/angular100Day/src/app/app.component.ts
@@ -6,23 +6,9 @@ import { ToggleComponent } from './toggle/toggle.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
-  checked = true;
-  @ViewChild(ToggleComponent) toggleComp!: ToggleComponent;
-  @ViewChild('nameInput', { static: true })
-  nameInput!: ElementRef<HTMLInputElement>;
-  @ViewChild('divElement')
-  divElement!: ElementRef<HTMLDivElement>;
-  @ViewChild('btn') btn!: ElementRef<HTMLButtonElement>;
-  ngAfterViewInit() {
-    this.divElement.nativeElement.innerText = 'Xin chào';
-    this.btn.nativeElement.onclick = () => {
-      this.toggleComp.toggle();
-    };
-  }
-  ngOnInit() {
-    setTimeout(() => {
-      this.nameInput.nativeElement.focus();
-    }, 3000);
-  }
+export class AppComponent {
+  question = {
+    question1: true,
+    question2: false,
+  };
 }
