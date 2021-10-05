11adf3aad44b9880962cebf6db794630aa7132f2
huongnvtv <huongitnv@gmail.com>
2021-10-05 09:19:47 +0700

    commit code project

--- front-end/learnAngular/codeProject/src/app/app.module.ts
+++ front-end/learnAngular/codeProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { PromoCodeComponent } from './promo-code/promo-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartSummaryComponent,
+    PromoCodeComponent
   ],
   imports: [
     BrowserModule,
