1f9005eb90b2bdfca7c69eb0cfa5d236c007a305
huongnvtv <huongitnv@gmail.com>
2021-10-24 19:09:59 +0700

    commit update form

--- front-end/learnAngular/projectManager/projectFrontEnd/src/app/employee-list/employee-list.component.ts
+++ front-end/learnAngular/projectManager/projectFrontEnd/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from '../model/employee';
 import { EmployeeService } from '../services/employee.service';
 
@@ -9,12 +10,18 @@ import { EmployeeService } from '../services/employee.service';
 })
 export class EmployeeListComponent implements OnInit {
   employees!: Employee[];
-  constructor(private employeeService: EmployeeService) {}
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.employeeService.getEmployeeList().subscribe((data) => {
       this.employees = data;
       console.log(this.employees);
     });
+  }
+  updateEmployee(id: number) {
+    this.router.navigate(['update-employee', id]);
   }
 }
