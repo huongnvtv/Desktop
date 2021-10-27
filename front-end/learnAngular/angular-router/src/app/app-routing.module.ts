96b450b2d90931cc5d90564c98c157833152e0a4
huongnvtv <huongitnv@gmail.com>
2021-10-27 15:49:31 +0700

    commit router

--- front-end/learnAngular/angular-router/src/app/app-routing.module.ts
+++ front-end/learnAngular/angular-router/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{ path: 'navigation', component: NavigationComponent }],
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
